f5f7e618f4f571f4999c89e3a98fc616647d8500
abdo2017 <abdo.esam2020@yandex.com>
2018-06-27 00:09:12 +0200

    added social,popular places,Gallary

--- js/main.js
+++ js/main.js
@@ -1,28 +1,56 @@
-$(function () {
-    //   init navbar
-    $(".button-collapse").sideNav();
+$(function() {
+  //   init navbar
+  $(".button-collapse").sideNav();
 
-    //  init slider
-    $(".slider").slider({
-        indicators: false,
-        height: $(window).height() - $("nav").height()
+  //  init slider
+  $(".slider").slider({
+    indicators: false,
+    height: $(window).height() - $("nav").height()
+  });
+
+  // the min-height image in the hero area
+  $(".slider .slides li").css({
+    minHeight: $(window).height() - $("nav").height()
+  });
+
+  $("input.autocomplete").autocomplete({
+    data: {
+      Apple: null,
+      Microsoft: null,
+      nokia: null,
+      "windows 10": null,
+      google: null,
+      facebook: null
+    },
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+    minLength: 1 // The minimum length of the input for the autocomplete to start. Default: 1.
+  });
+  var maxHeight = -1;
+  $(".feature .myCard").each(function() {
+    maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+  });
+
+  $(".feature .myCard").each(function() {
+      $(this).height(maxHeight);
+  });
+
+//  init the height for the popular places
+    var popheight = -1;
+    $(".section-popular .card").each(function() {
+        popheight = popheight > $(this).height() ? popheight : $(this).height();
     });
 
-    // the min-height image in the hero area
-    $(".slider .slides li").css({
-        minHeight: $(window).height() - $("nav").height()
+    $(".section-popular .card").each(function() {
+        $(this).height(popheight);
     });
 
-    $("input.autocomplete").autocomplete({
-        data: {
-            Apple: null,
-            Microsoft: null,
-            abdo: null,
-            nancy: null,
-            nelly: null,
-            Google: null
-        },
-        limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
-        minLength: 1 // The minimum length of the input for the autocomplete to start. Default: 1.
+    var imgHeight = -1;
+    $(".section-popular .card img").each(function() {
+        imgHeight = imgHeight > $(this).height() ? imgHeight : $(this).height();
     });
+
+    $(".section-popular .card img").each(function() {
+        $(this).height(imgHeight);
+    });
+
 });
